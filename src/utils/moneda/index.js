578f59df85c813f0d31abd7ee68ba70a29e09dd4
TomasAngeleri <tomi.angeleri94@gmail.com>
2023-03-23 00:29:16 -0300

    Updates

--- src/utils/moneda/index.js
+++
@@ -1,9 +0,0 @@
-const monedas = [
-  { id: 'USD', description: 'Dolar de Estados Unidos' },
-  { id: 'MXN', description: 'Peso Mexicano' },
-  { id: 'ARS', description: 'Peso Argentino' },
-  { id: 'EUR', description: 'Euro' },
-  { id: 'GBP', description: 'Libra Esterlina' },
-]
-
-export default monedas;
